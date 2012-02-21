8c61f6b20fd01cfb7fd31ad5a47d33f1a2fa09f9
James Hall <rio@x5g.com>
2012-02-21 09:57:58 +0000

    Working doc output

--- docx.js
+++ docx.js
@@ -42,37 +42,42 @@ var DOCXjs = function() {
 	}
 	
 	var documentGen = function() {
-		var output = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
 		
-		// Word document element with XML namespaces
-		output += '<w:wordDocument ';
-		output += ' xmlns:ve="http://schemas.openxmlformats.org/markup-compatibility/2006"'
-		output += ' xmlns:o="urn:schemas-microsoft-com:office:office"';
-		output += ' xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships"'
-		output += ' xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math"';
-		output += ' xmlns:v="urn:schemas-microsoft-com:vml"';
-		output += ' xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing"';
-		output += ' xmlns:w10="urn:schemas-microsoft-com:office:word" ';
-		output += ' xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main"';
-		output += ' xmlns:wne="http://schemas.microsoft.com/office/word/2006/wordml">';
-		
-		// Main body of the document
-		output += '<w:body>';
-		
-		for (var textElement in textElements) {
-			// Start a text section
-			output += '<w:p><w:r><w:rPr><w:rFonts w:ascii="Arial" w:hAnsi="Arial" w:cs="Arial"></w:rFonts><w:b w:val="single"></w:b></w:rPr>';
-			
-			// @TODO: Do escaping
-			output += '<w:t xml:space="preserve">' + textElements[textElement] + '</w:t></w:r></w:p>';
-		}
+		// Headers 
+		var output = '<w:document xmlns:ve="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing" xmlns:w10="urn:schemas-microsoft-com:office:word" xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:wne="http://schemas.microsoft.com/office/word/2006/wordml"><w:body>';
 		
 		
-		// Section 'sign-off', dimensions, gutter etc.
-		output +='<w:sectPr w:rsidR="12240" w:rsidRPr="12240" w:rsidSect="12240"><w:pgSz w:w="11906" w:h="16838"></w:pgSz><w:pgMar w:top="1440" w:right="1800" w:bottom="1440" w:left="1800" w:header="0" w:footer="0" w:gutter="0"></w:pgMar><w:cols w:space="708"></w:cols><w:docGrid w:linePitch="360"></w:docGrid></w:sectPr>';
+		// Paragraphs
 		
-		// Close
-		output += '</w:body></w:wordDocument>';
+		output += '<w:p w:rsidR="001A6335" w:rsidRDefault="00EA68DC" w:rsidP="00EA68DC">';
+		output += '<w:r w:rsidRPr="00C703AC">';
+		output += '<w:rPr>';
+		output += '<w:lang w:val="es-ES_tradnl"/>';
+		output += '</w:rPr>';
+		output += '<w:t xml:space="preserve">';
+		output += 'Fusce blandit, augue eu luctus posuere, mi arcu consequat lorem, non volutpat neque enim a mi. Aliquam nonummy justo in purus. Quisque dolor purus, auctor in, varius non, auctor quis, dolor. Mauris vitae elit. Suspendisse dolor ante, imperdiet id, sodales auctor, tempus mollis, nisl. Donec vehicula tincidunt lectus. Donec cursus. Nam tempor pede eu urna. Praesent nec magna at enim ornare posuere. Fusce pharetra dapibus ligula.';
+		output += '</w:t>';
+		output += '</w:r>';
+		output += '</w:p>';
+		
+		
+		
+		// Bottom section
+		output += '<w:sectPr w:rsidR="001A6335" w:rsidSect="001A6335">';
+		output += '<w:headerReference w:type="even" r:id="rId6"/>';
+		output += '<w:headerReference w:type="default" r:id="rId7"/>';
+		output += '<w:footerReference w:type="even" r:id="rId8"/>';
+		output += '<w:footerReference w:type="default" r:id="rId9"/>';
+		output += '<w:headerReference w:type="first" r:id="rId10"/>';
+		output += '<w:footerReference w:type="first" r:id="rId11"/>';
+		output += '<w:pgSz w:w="12240" w:h="15840"/>';
+		output += '<w:pgMar w:top="1440" w:right="1800" w:bottom="1440" w:left="1800" w:header="720" w:footer="720" w:gutter="0"/>';
+		output += '<w:cols w:space="720"/>';
+		output += '<w:docGrid w:linePitch="360"/>';
+		output += '</w:sectPr>';
+		
+		// Close 
+		output += '</w:body></w:document>';
 		
 		return output;
 	}
@@ -109,23 +114,36 @@ var DOCXjs = function() {
 		var file_count_current = 0;
 		var zip = new JSZip("STORE");
 				
+		var doOutput = function() {
+			outputFile = zip.generate();
+			document.location.href = 'data:application/vnd.openxmlformats-officedocument.wordprocessingml.document;base64,' + outputFile;
+		}
+		
 		for(var file in files) {
-			$.ajax({
-				url: 'blank/' + files[file],
-				complete: function(r) {
-					//file_data[this.url.replace(/blank_/, '')] = r.responseText;
-					
-					zip.add(this.url.replace('blank/', ''), r.responseText);
-					file_count_current ++;
-					
-					if (file_count == file_count_current) {
-						console.log('DONE!');
-						outputFile = zip.generate();
-						document.location.href = 'data:application/vnd.openxmlformats-officedocument.wordprocessingml.document;base64,' + outputFile;
-					}
-					
+			console.log(file);
+			if (files[file] == 'word/document.xml') {
+				zip.add('word/document.xml', documentGen());
+				file_count_current ++;
+
+				if (file_count == file_count_current) {							
+					doOutput();
 				}
-			});
+			} else {
+				$.ajax({
+					url: 'blank/' + files[file],
+					complete: function(r) {
+						//file_data[this.url.replace(/blank_/, '')] = r.responseText;
+
+						zip.add(this.url.replace('blank/', ''), r.responseText);
+						file_count_current ++;
+
+						if (file_count == file_count_current) {							
+							doOutput();
+						}
+					}
+				});
+			}
+
 		}		
 		
 		return parts;
